ddaacb535a7c0c7e560ad54c26b45324cc60283d
Malte Hübner <maltehuebner@gmx.org>
2017-01-10 02:59:54 +0100

    Add facebook login properties.

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -7,7 +7,7 @@ use Doctrine\ORM\Mapping as ORM;
 
 /**
  * @ORM\Entity
- * @ORM\Table(name="fos_user")
+ * @ORM\Table(name="user")
  */
 class User extends BaseUser
 {
@@ -18,9 +18,42 @@ class User extends BaseUser
      */
     protected $id;
 
+    /**
+     * @ORM\Column(name="facebook_id", type="string", length=255, nullable=true)
+     */
+    protected $facebookId;
+    /**
+     * @ORM\Column(name="facebook_access_token", type="string", length=255, nullable=true)
+     */
+    protected $facebookAccessToken;
+
     public function __construct()
     {
         parent::__construct();
         // your own logic
+    }
+
+    public function setFacebookId(string $facebookId): User
+    {
+        $this->facebookId = $facebookId;
+
+        return $this;
+    }
+
+    public function getFacebookId(): ?string
+    {
+        return $this->facebookId;
+    }
+
+    public function setFacebookAccessToken(string $facebookAccessToken): User
+    {
+        $this->facebookAccessToken = $facebookAccessToken;
+
+        return $this;
+    }
+
+    public function getFacebookAccessToken(): ?string
+    {
+        return $this->facebookAccessToken;
     }
 }

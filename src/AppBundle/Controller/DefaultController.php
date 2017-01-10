cd78c18989c1f579780c4a172902c062348f94f8
Malte Hübner <maltehuebner@gmx.org>
2017-01-10 01:42:08 +0100

    Show list of uploaded photos at front page.

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -13,9 +13,10 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
+        $photoList = $this->getDoctrine()->getRepository('AppBundle:Photo')->findAll();
+
+        return $this->render('AppBundle:Default:index.html.twig', [
+            'photoList' => $photoList
         ]);
     }
 }

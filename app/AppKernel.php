2bfdd651d4d21f25bb15ff2b0bd0749431e74272
Malte Hübner <maltehuebner@gmx.org>
2017-01-10 01:58:57 +0100

    Require and activate FOSUserBundle.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new \Vich\UploaderBundle\VichUploaderBundle(),
             new Liip\ImagineBundle\LiipImagineBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
             new AppBundle\AppBundle(),
         ];
 

92ccfcaa61192a0fe60bcd4f403b78096782089b
Malte Hübner <maltehuebner@gmx.org>
2017-01-10 01:08:43 +0100

    Copy Photo entity from criticalmass.

---
+++ src/AppBundle/Entity/Photo.php
@@ -0,0 +1,200 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\HttpFoundation\File\File;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\Validator\Constraints as Assert;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
+
+/**
+ * @ORM\Table(name="photo")
+ * @Vich\Uploadable
+ */
+class Photo
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @ORM\Column(type="float", nullable=true)
+     */
+    protected $latitude;
+
+    /**
+     * @ORM\Column(type="float", nullable=true)
+     */
+    protected $longitude;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    protected $description;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    protected $enabled = true;
+
+    /**
+     * @ORM\Column(type="datetime")
+     * @JMS\Expose
+     */
+    protected $dateTime;
+
+    /**
+     * @ORM\Column(type="datetime")
+     * @JMS\Expose
+     */
+    protected $updatedAt;
+
+    /**
+     * @ORM\Column(type="datetime")
+     * @JMS\Expose
+     */
+    protected $creationDateTime;
+
+    /**
+     * @Vich\UploadableField(mapping="photo", fileNameProperty="imageName")
+     *
+     * @var File
+     */
+    protected $imageFile;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     *
+     * @var string
+     */
+    protected $imageName;
+
+    public function __construct()
+    {
+        $this->dateTime = new \DateTime();
+        $this->creationDateTime = new \DateTime();
+        $this->updatedAt = new \DateTime();
+        $this->description = '';
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEnabled(): bool
+    {
+        return $this->enabled;
+    }
+
+    public function setEnabled(bool $enabled): Photo
+    {
+        $this->enabled = $enabled;
+
+        return $this;
+    }
+
+    public function getLatitude(): ?float
+    {
+        return $this->latitude;
+    }
+
+    public function setLatitude(float $latitude): Photo
+    {
+        $this->latitude = $latitude;
+
+        return $this;
+    }
+
+    public function getLongitude(): ?float
+    {
+        return $this->longitude;
+    }
+
+    public function setLongitude(float $longitude): Photo
+    {
+        $this->longitude = $longitude;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): Photo
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getDateTime(): ?\DateTime
+    {
+        return $this->dateTime;
+    }
+
+    public function setDateTime(\Datetime $dateTime): Photo
+    {
+        $this->dateTime = $dateTime;
+
+        return $this;
+    }
+
+    public function getCreationDateTime(): \DateTime
+    {
+        return $this->creationDateTime;
+    }
+
+    public function setCreationDateTime(\DateTime $creationDateTime): Photo
+    {
+        $this->creationDateTime = $creationDateTime;
+
+        return $this;
+    }
+
+    public function setImageFile(UploadedFile $image = null): Photo
+    {
+        $this->imageFile = $image;
+
+        if ($image) {
+            $this->updatedAt = new \DateTime('now');
+        }
+
+        return $this;
+    }
+
+    public function getImageFile(): ?File
+    {
+        return $this->imageFile;
+    }
+
+    public function setImageName(string $imageName): Photo
+    {
+        $this->imageName = $imageName;
+
+        return $this;
+    }
+
+    public function getImageName(): ?string
+    {
+        return $this->imageName;
+    }
+
+    public function setUpdatedAt(\DateTime $updatedAt): Photo
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTime
+    {
+        return $this->updatedAt;
+    }
+}

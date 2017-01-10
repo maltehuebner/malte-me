73e0dad6ef1ffde6bcfe4efb8f9c9666e0424533
Malte Hübner <maltehuebner@gmx.org>
2017-01-10 21:00:03 +0100

    Add title and description to form type.

--- src/AppBundle/Form/Type/PhotoType.php
+++ src/AppBundle/Form/Type/PhotoType.php
@@ -4,6 +4,7 @@ namespace AppBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Vich\UploaderBundle\Form\Type\VichFileType;
 
@@ -12,6 +13,8 @@ class PhotoType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
+            ->add('title', TextType::class, ['required' => true])
+            ->add('description', TextType::class, ['required' => false])
             ->add('imageFile', VichFileType::class, ['required' => true])
             ->add('submit', SubmitType::class)
         ;

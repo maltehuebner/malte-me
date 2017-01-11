23ae63e898b2ba87da31ad0534451ef676c3dfa4
Malte Hübner <maltehuebner@gmx.org>
2017-01-11 01:09:08 +0100

    Create and save slug during upload.

--- src/AppBundle/Controller/PhotoController.php
+++ src/AppBundle/Controller/PhotoController.php
@@ -8,6 +8,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Core\User\UserInterface;
+use \Malenki\Slug;
 
 class PhotoController extends Controller
 {
@@ -23,12 +24,19 @@ class PhotoController extends Controller
         $uploadForm->handleRequest($request);
 
         if ($uploadForm->isSubmitted() && $uploadForm->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+
             /** @var Photo $photo */
             $photo = $uploadForm->getData();
-            $photo->setUser($user);
 
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($photo);
+            $photo->setUser($user);
+            $em->persist($photo); // first persist to generate id
+            $em->flush();
+
+            $slug = new Slug($photo->getTitle().' '.$photo->getId());
+            $photo->setSlug($slug);
+
+            $em->persist($photo); // second persist to save slug
             $em->flush();
         }
 

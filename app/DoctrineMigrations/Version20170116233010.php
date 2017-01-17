3c62c1e805d86605471880467ee9c4a95bf2931e
Malte Hübner <maltehuebner@gmx.org>
2017-01-18 00:22:59 +0100

    Hotfix for shitty shit.

--- app/DoctrineMigrations/Version20170116233010.php
+++ app/DoctrineMigrations/Version20170116233010.php
@@ -18,7 +18,7 @@ class Version20170116233010 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE photo ADD highlighted TINYINT(1) NOT NULL, ADD sponsored TINYINT(1) NOT NULL, ADD affiliated TINYINT(1) NOT NULL, CHANGE creation_date_time created_at DATETIME NOT NULL');
+        $this->addSql('ALTER TABLE photo ADD highlighted TINYINT(1) NOT NULL, ADD sponsored TINYINT(1) NOT NULL, ADD affiliated TINYINT(1) NOT NULL');
     }
 
     /**
@@ -29,6 +29,6 @@ class Version20170116233010 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE photo DROP highlighted, DROP sponsored, DROP affiliated, CHANGE created_at creation_date_time DATETIME NOT NULL');
+        $this->addSql('ALTER TABLE photo DROP highlighted, DROP sponsored, DROP affiliated');
     }
 }

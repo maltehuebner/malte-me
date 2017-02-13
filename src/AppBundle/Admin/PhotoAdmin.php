ac94e2983ad823f634811cf4f06eeaf7eb3cb90a
Malte Hübner <maltehuebner@gmx.org>
2017-02-13 21:48:27 +0100

    More WIP for PhotoAdmin.

--- src/AppBundle/Admin/PhotoAdmin.php
+++ src/AppBundle/Admin/PhotoAdmin.php
@@ -6,21 +6,39 @@ use Sonata\AdminBundle\Admin\AbstractAdmin;
 use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Sonata\AdminBundle\Form\FormMapper;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Vich\UploaderBundle\Form\Type\VichFileType;
 
 class PhotoAdmin extends AbstractAdmin
 {
     protected function configureFormFields(FormMapper $formMapper)
     {
-        $formMapper->add('name', 'text');
+        $formMapper
+            ->add('slug', TextType::class)
+            ->add('title', TextType::class)
+            ->add('description', TextareaType::class)
+            ->add('enabled', CheckboxType::class)
+            ->add('highlighted', CheckboxType::class)
+            ->add('sponsored', CheckboxType::class)
+            ->add('affiliated', CheckboxType::class)
+            ->add('dateTime', DateTimeType::class)
+            ->add('displayDateTime', DateTimeType::class)
+            ->add('updatedAt', DateTimeType::class)
+            ->add('createdAt', DateTimeType::class)
+            ->add('imageFile', VichFileType::class)
+        ;
     }
 
     protected function configureDatagridFilters(DatagridMapper $datagridMapper)
     {
-        $datagridMapper->add('name');
+        $datagridMapper->add('title');
     }
 
     protected function configureListFields(ListMapper $listMapper)
     {
-        $listMapper->addIdentifier('name');
+        $listMapper->addIdentifier('title');
     }
 }

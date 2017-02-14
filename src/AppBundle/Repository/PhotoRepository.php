8b1e80b62e8e6e20a636c3af02f2c733e4965b8c
Malte Hübner <maltehuebner@gmx.org>
2017-02-14 21:38:54 +0100

    Extract query method from repository.

--- src/AppBundle/Repository/PhotoRepository.php
+++ src/AppBundle/Repository/PhotoRepository.php
@@ -3,10 +3,18 @@
 namespace AppBundle\Repository;
 
 use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\Query;
 
 class PhotoRepository extends EntityRepository
 {
     public function findForFrontpage(): array
+    {
+        $query = $this->getFrontpageQuery();
+
+        return $query->getResult();
+    }
+
+    public function getFrontpageQuery(): Query
     {
         $qb = $this->createQueryBuilder('p');
 
@@ -14,8 +22,6 @@ class PhotoRepository extends EntityRepository
 
         $qb->addOrderBy('p.displayDateTime', 'DESC');
 
-        $query = $qb->getQuery();
-
-        return $query->getResult();
+        return $qb->getQuery();
     }
 }

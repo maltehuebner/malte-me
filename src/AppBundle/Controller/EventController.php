08258aced80e61ae808600aac42d89d535b5f4bd
Malte Hübner <maltehuebner@gmx.org>
2017-05-01 22:19:53 +0200

    Show a list of events.

--- src/AppBundle/Controller/EventController.php
+++ src/AppBundle/Controller/EventController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller;
 
+use Curl\Curl;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,9 +11,13 @@ class EventController extends Controller
 {
     public function listAction(Request $request): Response
     {
+        $curl = new Curl();
+        $curl->get($this->getParameter('cyclingcities.api.event_list'));
+
+        $eventList = json_decode($curl->response);
+
         return $this->render('AppBundle:Event:list.html.twig', [
+            'eventList' => $eventList
         ]);
     }
-
-
 }

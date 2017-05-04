1cd557d074e303f49a0592b518d53aa36739c5af
Malte Hübner <maltehuebner@gmx.org>
2017-05-04 16:05:11 +0200

    Post comments to its photo.

--- src/AppBundle/Resources/public/js/modules/CommentBox.js
+++ src/AppBundle/Resources/public/js/modules/CommentBox.js
@@ -1,7 +1,20 @@
 define([], function () {
     CommentBox = function (context, options) {
         $('.photo').each(function() {
-            console.log($(this).data('photo-id'));
+            console.log();
+        });
+
+        $('#comment-modal').on('show.bs.modal', function (event) {
+            var $button = $(event.relatedTarget);
+            var $photo = $button.closest('.photo');
+            var photoId = $photo.data('photo-id');
+
+            var route = Routing.generate('photo_add_comment', { photoId: photoId });
+
+            var $modal = $(this);
+            var $form = $modal.find('form');
+
+            $form.attr('action', route);
         });
     };
 

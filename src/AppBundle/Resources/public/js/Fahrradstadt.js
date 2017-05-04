6fe2fda228798a901696b581fe13c59cec3b717f
Malte Hübner <maltehuebner@gmx.org>
2017-05-04 14:56:39 +0200

    Add Fahrradstadt.js

---
+++ src/AppBundle/Resources/public/js/Fahrradstadt.js
@@ -0,0 +1,19 @@
+var Fahrradstadt = Fahrradstadt || {};
+
+Fahrradstadt.loadModule = function(name, context, options, callback) {
+    require([name], function(Module) {
+        var module = new Module(context, options);
+
+        if (callback) {
+            callback(module);
+        }
+    });
+};
+
+require.config({
+    baseUrl: '/bundles/app/',
+    paths:
+    {
+
+    }
+});

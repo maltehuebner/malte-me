8d93aa8b94f3c82536ccd12fb9591b6cb508bf85
Malte Hübner <maltehuebner@gmx.org>
2017-05-04 19:22:46 +0200

    Do ajax.

--- src/AppBundle/Resources/public/js/modules/FavoriteStar.js
+++ src/AppBundle/Resources/public/js/modules/FavoriteStar.js
@@ -1,6 +1,13 @@
 define([], function () {
     FavoriteStar = function (context, options) {
+        $('.favorite-star').click(function(e) {
+            e.preventDefault();
 
+            var $star = $(this);
+            var url = $star.attr('href');
+
+            $.get(url);
+        });
     };
 
     return FavoriteStar;

755179fede42fd2ef7d025e49b094f21f3dcf0e5
Malte Hübner <maltehuebner@gmx.org>
2017-05-10 23:36:47 +0200

    Add plain feed without images.

--- src/AppBundle/Controller/FeedController.php
+++ src/AppBundle/Controller/FeedController.php
@@ -4,20 +4,25 @@ namespace AppBundle\Controller;
 
 use AppBundle\Entity\Photo;
 use cebe\markdown\Markdown;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Suin\RSSWriter\Channel;
 use Suin\RSSWriter\Feed;
 use Suin\RSSWriter\Item;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Security\Core\User\UserInterface;
 
 class FeedController extends Controller
 {
-    public function indexAction(Request $request, UserInterface $user = null): Response
+    public function indexAction(Request $request): Response
     {
         $feed = $this->buildFeed();
+
+        return new Response($feed);
+    }
+
+    public function plainAction(Request $request): Response
+    {
+        $feed = $this->buildFeed(false);
 
         return new Response($feed);
     }

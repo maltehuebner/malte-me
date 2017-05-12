66485f138accec54f465e30fea0dfb584bb6afb9
Malte Hübner <maltehuebner@gmx.org>
2017-05-12 14:33:40 +0200

    Add edit action.

--- src/AppBundle/Controller/PhotoController.php
+++ src/AppBundle/Controller/PhotoController.php
@@ -3,18 +3,17 @@
 namespace AppBundle\Controller;
 
 use AppBundle\Entity\Photo;
-use AppBundle\Form\Type\PhotoType;
-use PHPExif\Reader\Reader;
+use AppBundle\Form\Type\PhotoEditType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\User\UserInterface;
-use \Malenki\Slug;
 
 class PhotoController extends Controller
 {
     public function viewAction(Request $request, UserInterface $user = null, string $slug): Response
     {
+        /** @var Photo $photo */
         $photo = $this->getDoctrine()->getRepository('AppBundle:Photo')->findOneBySlug($slug);
 
         if (!$photo) {
@@ -37,6 +36,36 @@ class PhotoController extends Controller
             'photo' => $photo,
             'comments' => $comments,
             'userFavorites' => $userFavorites,
+        ]);
+    }
+
+    public function editAction(Request $request, UserInterface $user = null, int $photoId): Response
+    {
+        /** @var Photo $photo */
+        $photo = $this->getDoctrine()->getRepository('AppBundle:Photo')->find($photoId);
+
+        if (!$photo) {
+            throw $this->createNotFoundException();
+        }
+
+        $editForm = $this->createForm(PhotoEditType::class, $photo);
+
+        $editForm->handleRequest($request);
+
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+
+            /** @var Photo $photo */
+            $photo = $editForm->getData();
+
+            $em->flush();
+
+            return $this->redirectToRoute('show_photo', ['slug' => $photo->getSlug()]);
+        }
+
+        return $this->render('AppBundle:Photo:edit.html.twig', [
+            'photo' => $photo,
+            'editForm' => $editForm->createView(),
         ]);
     }
 }

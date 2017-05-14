5444149abaab0a7f566cd7501f39b1df38a6c5dd
Malte Hübner <maltehuebner@gmx.org>
2017-05-14 09:24:30 +0200

    WIP.

--- src/AppBundle/Controller/SearchController.php
+++ src/AppBundle/Controller/SearchController.php
@@ -10,7 +10,18 @@ class SearchController extends Controller
 {
     public function searchAction(Request $request): Response
     {
-        $pagination = [];
+        $finder = $this->container->get('fos_elastica.finder.fahrradstadt.photo');
+        $page = $request->query->getInt('page', 1);
+        $query = $request->query->get('query');
+
+        $paginator = $this->get('knp_paginator');
+        $results = $finder->createPaginatorAdapter($query);
+
+        $pagination = $paginator->paginate(
+            $results,
+            $page,
+            50
+        );
 
         return $this->render('AppBundle:Search:results.html.twig', [
             'pagination' => $pagination

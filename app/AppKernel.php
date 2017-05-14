14e09387f344ef746d17acbda5c6072365cff7f1
Malte Hübner <maltehuebner@gmx.org>
2017-05-14 09:02:10 +0200

    Activate FosElasticaBundle.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -27,6 +27,7 @@ class AppKernel extends Kernel
             new Sonata\AdminBundle\SonataAdminBundle(),
             new Knp\Bundle\PaginatorBundle\KnpPaginatorBundle(),
             new FOS\JsRoutingBundle\FOSJsRoutingBundle(),
+            new FOS\ElasticaBundle\FOSElasticaBundle(),
             new AppBundle\AppBundle(),
             new UserBundle\UserBundle()
         ];

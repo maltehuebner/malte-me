6402df4533a01b4c0645420c08ed0dceafd649f1
Malte Hübner <maltehuebner@gmx.org>
2017-06-27 14:44:41 +0200

    Refactoring.

--- src/AppBundle/Resources/public/js/modules/LocationBox.js
+++ src/AppBundle/Resources/public/js/modules/LocationBox.js
@@ -1,19 +1,47 @@
 define(['leaflet'], function () {
     LocationBox = function (context, options) {
-        $('#location-modal').on('shown.bs.modal', function (event) {
-            var map = L.map('location-map').setView([51.505, -0.09], 13);
+        this._initModalEventListener(context);
+    };
 
-            L.tileLayer('https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}.png', {
-                attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-            }).addTo(map);
+    LocationBox.prototype._standardCenter = [53.550757, 9.993010];
+    LocationBox.prototype._map = null;
+    LocationBox.prototype._marker = null;
 
-            L.marker([51.5, -0.09]).addTo(map)
-                .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-                .openPopup();
+    LocationBox.prototype._initModalEventListener = function(context) {
+        var that = this;
+
+        $(context).on('shown.bs.modal', function (event) {
+            var $button = $(event.relatedTarget);
+            var $photo = $button.closest('.photo');
+            var latitude = $photo.data('latitude');
+            var longitude = $photo.data('longitude');
+
+            var center = that._standardCenter;
+
+            if (latitude && longitude) {
+                center = [latitude, longitude];
+            }
+
+            that._initMap(center);
+            that._initMarker(center);
         });
     };
 
-    LocationBox.prototype._standardLatLng = [57, 10];
+    LocationBox.prototype._initMap = function(center) {
+        this._map = L.map('location-map').setView(center, 13);
+
+        L.tileLayer('https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(this._map);
+    };
+
+    LocationBox.prototype._initMarker = function(center) {
+        this._marker = L.marker(center, {
+            draggable: true
+        }).addTo(this._map)
+            .bindPopup('Wo hast du das Foto geschossen?')
+            .openPopup();
+    };
 
     return LocationBox;
 });

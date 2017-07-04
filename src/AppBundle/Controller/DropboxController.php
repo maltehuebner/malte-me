719e4c2ac582e8e867b2c19a57d2678bedc925d1
Malte Hübner <maltehuebner@gmx.org>
2017-07-04 23:59:09 +0200

    WIP.

--- src/AppBundle/Controller/DropboxController.php
+++ src/AppBundle/Controller/DropboxController.php
@@ -2,8 +2,12 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Photo;
+use AppBundle\PhotoUploader\PhotoUploader;
 use Kunnu\Dropbox\Dropbox;
 use Kunnu\Dropbox\DropboxApp;
+use Kunnu\Dropbox\Models\FileMetadata;
+use Kunnu\Dropbox\Models\ModelCollection;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -11,7 +15,7 @@ use Symfony\Component\Security\Core\User\UserInterface;
 
 class DropboxController extends Controller
 {
-    public function indexAction(Request $request, UserInterface $user = null): Response
+    public function indexAction(Request $request, UserInterface $user): Response
     {
         $clientId = $this->getParameter('dropbox.client_id');
         $clientSecret = $this->getParameter('dropbox.client_secret');
@@ -23,27 +27,47 @@ class DropboxController extends Controller
 
         $listFolderContents = $dropbox->listFolder('/');
 
+        /** @var ModelCollection $items */
         $items = $listFolderContents->getItems();
 
+        /** @var FileMetadata $item */
         foreach ($items as $item) {
-            $file = $dropbox->download($item->getId());
-
-//File Contents
-            $contents = $file->getContents();
-
-
-//Save file contents to disk
-            //file_put_contents(__DIR__ . "/logo.png", $contents);
-
-//Downloaded File Metadata
-            $metadata = $file->getMetadata();
-
-//Name
-            $metadata->getName();
-
-            var_dump($metadata);
+            $photo = $this->importFile($dropbox, $item, $user);
         }
 
         return new Response();
+    }
+
+    protected function getFileSuffix(FileMetadata $fileMetadata): string
+    {
+        $filenameParts = explode('.', $fileMetadata->getName());
+
+        return array_pop($filenameParts);
+    }
+
+    protected function importFile(Dropbox $dropbox, FileMetadata $fileMetadata, UserInterface $user): Photo
+    {
+        $uploadPath = $this->getParameter('upload_destination.photo');
+        $tmpFilename = '/tmp/fahrradstadt-dropbox-import';
+
+        $filename = sprintf('%s.%s', uniqid(), $this->getFileSuffix($fileMetadata));
+        $path = sprintf('%s/%s', $uploadPath, $filename);
+
+        $file = $dropbox->download($fileMetadata->getPathLower());
+        file_put_contents($path, $file->getContents());
+
+        /** @var PhotoUploader $photoUploader */
+        $photoUploader = $this->get('app.photo_uploader');
+
+        $photo = new Photo();
+
+        $photo
+            ->setUser($user)
+            ->setImageName($filename)
+        ;
+
+        $photo = $photoUploader->handleUpload($photo, $user);
+
+        return $photo;
     }
 }

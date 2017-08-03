2c836664138b9340b9b26496e614ffc8e097c869
Malte Hübner <maltehuebner@gmx.org>
2017-08-03 21:55:07 +0200

    Get SeoPage.

--- src/AppBundle/Controller/AbstractController.php
+++ src/AppBundle/Controller/AbstractController.php
@@ -2,8 +2,13 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Seo\SeoPage;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 abstract class AbstractController extends Controller
 {
+    protected function getSeoPage(): SeoPage
+    {
+        return $this->get('app.seo_page');
+    }
 }

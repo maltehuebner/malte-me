c327062b2e4fcd6ba740375ed3c0a2e8983b4382
Malte Hübner <maltehuebner@gmx.org>
2017-08-05 22:34:17 +0200

    Added more properties to City entity.

--- src/AppBundle/Entity/City.php
+++ src/AppBundle/Entity/City.php
@@ -5,9 +5,9 @@ namespace AppBundle\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
-use Symfony\Component\CssSelector\Node\Specificity;
+use Symfony\Component\HttpFoundation\File\File;
 use Symfony\Component\Validator\Constraints as Assert;
-
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
 /**
  * @ORM\Table(name="city")
  * @ORM\Entity()
@@ -38,6 +38,42 @@ class City
      * @Assert\NotBlank()
      */
     protected $slug;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=false)
+     * @Assert\NotBlank()
+     */
+    protected $hostname;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=false)
+     * @Assert\NotBlank()
+     */
+    protected $seoDescription;
+
+    /**
+     * @Vich\UploadableField(mapping="photo", fileNameProperty="imageName")
+     *
+     * @var File
+     */
+    protected $imageFile;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     *
+     * @var string
+     */
+    protected $imageName;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    protected $updatedAt;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    protected $createdAt;
 
     /**
      * @ORM\Column(type="boolean")
@@ -114,6 +150,89 @@ class City
     public function setEnabled(bool $enabled): City
     {
         $this->enabled = $enabled;
+
+        return $this;
+    }
+
+    public function getHostname(): ?string
+    {
+        return $this->hostname;
+    }
+
+    public function setHostname(string $hostname): City
+    {
+        $this->hostname = $hostname;
+
+        return $this;
+    }
+
+    public function getSeoDescription(): ?string
+    {
+        return $this->seoDescription;
+    }
+
+    public function setSeoDescription(string $seoDescription): City
+    {
+        $this->seoDescription = $seoDescription;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): \DateTime
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTime $createdAt): City
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function setImageFile(File $image = null): City
+    {
+        $this->imageFile = $image;
+
+        if ($image) {
+            $this->updatedAt = new \DateTime('now');
+        }
+
+        return $this;
+    }
+
+    public function getImageFile(): ?File
+    {
+        return $this->imageFile;
+    }
+
+    public function setImageName(string $imageName = null): City
+    {
+        $this->imageName = $imageName;
+
+        return $this;
+    }
+
+    public function getImageName(): ?string
+    {
+        return $this->imageName;
+    }
+
+    public function setUpdatedAt(\DateTime $updatedAt): City
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTime
+    {
+        return $this->updatedAt;
+    }
+
+    public function setSponsored(bool $sponsored): City
+    {
+        $this->sponsored = $sponsored;
 
         return $this;
     }

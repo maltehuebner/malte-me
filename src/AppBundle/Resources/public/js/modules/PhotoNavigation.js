d0300c6afbbed9c7165ebe7071bc7a0d6c32053a
Malte Hübner <maltehuebner@gmx.org>
2017-08-18 00:35:41 +0200

    Done.

--- src/AppBundle/Resources/public/js/modules/PhotoNavigation.js
+++ src/AppBundle/Resources/public/js/modules/PhotoNavigation.js
@@ -1,23 +1,49 @@
 define([], function () {
     PhotoNavigation = function (context, options) {
-        this._initEventListeners();
+        this._initEventListeners(context);
     };
 
-    PhotoNavigation.prototype._initEventListeners = function() {
+    PhotoNavigation.prototype._initEventListeners = function(selector) {
         var that = this;
 
-        $('img').click(function (e) { //Relative ( to its parent) mouse position
+        $(selector).click(function (e) {
             e.preventDefault();
 
             var clickX = e.pageX - $(this).position().left;
             var imgWidth = $(this).width();
 
             if (imgWidth / 2 < clickX) {
-                alert('rechts');
+                that._getNextPhotoAddress();
             } else {
-                alert('links');
+                that._getPreviousPhotoAddress();
             }
         });
+    };
+
+    PhotoNavigation.prototype._getPreviousPhotoAddress = function() {
+        var url = this._getButtonHref($('#button-previous-photo'));
+
+        if (url) {
+            window.location = url;
+        }
+    };
+
+    PhotoNavigation.prototype._getNextPhotoAddress = function() {
+        var url = this._getButtonHref($('#button-next-photo'));
+
+        if (url) {
+            window.location = url;
+        }
+    };
+
+    PhotoNavigation.prototype._getButtonHref = function($element) {
+        var url = null;
+
+        if ($element) {
+            url = $element.attr('href');
+        }
+        
+        return url;
     };
 
     return PhotoNavigation;

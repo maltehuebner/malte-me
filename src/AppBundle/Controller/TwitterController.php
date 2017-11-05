28ba2a733e3d39d0aa8dcb169f5fd5dee3856499
Malte Hübner <maltehuebner@gmx.org>
2017-11-05 02:47:25 +0100

    Implement test route.

---
+++ src/AppBundle/Controller/TwitterController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class TwitterController extends AbstractController
+{
+    public function postAction(Request $request): Response
+    {
+
+        return new Response();
+    }
+}

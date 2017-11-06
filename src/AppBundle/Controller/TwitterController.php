26fa4efef83c60d4fe2f71f8496880a3c6558c4f
Malte Hübner <maltehuebner@gmx.org>
2017-11-06 21:41:58 +0100

    WIP.

--- src/AppBundle/Controller/TwitterController.php
+++ src/AppBundle/Controller/TwitterController.php
@@ -2,10 +2,13 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\City;
 use Codebird\Codebird;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\Routing\RouterInterface;
 
 class TwitterController extends AbstractController
 {
@@ -26,66 +29,43 @@ class TwitterController extends AbstractController
 
         $cb = $this->getCodeBird();
 
-        if (!$session->get('oauth_token')) {
-            // get the request token
-            $reply = $cb->oauth_requestToken([
-                'oauth_callback' => 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']
-            ]);
+        $callbackUrl = $this->generateUrl('twitter_token', [], RouterInterface::ABSOLUTE_URL);
 
-            // store the token
-            $cb->setToken($reply->oauth_token, $reply->oauth_token_secret);
+        // get the request token
+        $reply = $cb->oauth_requestToken([
+            'oauth_callback' => $callbackUrl,
+        ]);
 
-            $session->set('oauth_token', $reply->oauth_token);
-            $session->set('oauth_token_secret', $reply->oauth_token_secret);
-            $session->set('oauth_verify', true);
+        var_dump($reply);
+        if ($reply->oauth_callback_confirmed) {
+            $this->saveCityAccess($request, $reply);
 
-            // redirect to auth website
-            $auth_url = $cb->oauth_authorize();
-            header('Location: ' . $auth_url);
-            die();
-
-        } elseif ($request->request->has('oauth_verifier') && $session->has('oauth_verify')) {
-            // verify the token
-            $cb->setToken($session->get('oauth_token'), $session->get('oauth_token_secret'));
-            $session->remove('oauth_verify');
-
-            // get the access token
-            $reply = $cb->oauth_accessToken([
-                'oauth_verifier' => $session->get('oauth_verifier')
-            ]);
-
-            // store the token (which is different from the request token!)
-            $session->set('oauth_token', $reply->oauth_token);
-            $session->set('oauth_token_secret', $reply->oauth_token_secret);
-
-            var_dump($reply);
+            return new Response('gespeichert');
+        } else {
+            return new RedirectResponse($cb->oauth_authorize);
         }
-
-// assign access token on each page load
-        $cb->setToken($session->get('oauth_token'), $session->get('oauth_token_secret'));
-
-        $session->remove('oauth_token');
-        $session->remove('oauth_token_secret');
-
-        return new Response('');
     }
 
     public function tokenAction(Request $request): Response
     {
-        $token = $this->getSession()->get('oauth_token');
-        $tokenSecret = $this->getSession()->get('oauth_token_secret');
+        $session = $this->getSession();
+        $cb = $this->getCodeBird();
 
-        $city = $this->getCity($request);
+        if ($request->request->has('oauth_verifier') && $session->has('oauth_verify')) {
+            $session->remove('oauth_verify');
 
-        $city
-            ->setTwitterToken($token)
-            ->setTwitterSecret($tokenSecret)
-        ;
+            $reply = $cb->oauth_accessToken([
+                'oauth_verifier' => $session->get('oauth_verifier')
+            ]);
 
-        $this->getDoctrine()->getManager()->flush();
+
+
+            var_dump($reply);
+        }
 
         return new Response('Gespeichert');
     }
+
     protected function getCodeBird(): Codebird
     {
         Codebird::setConsumerKey($this->getParameter('twitter.client_id'), $this->getParameter('twitter.client_secret'));
@@ -96,5 +76,19 @@ class TwitterController extends AbstractController
     protected function getSession(): Session
     {
         return $this->get('session');
+    }
+
+    protected function saveCityAccess(Request $request, \stdClass $reply): City
+    {
+        $city = $this->getCity($request);
+
+        $city
+            ->setTwitterToken($reply->oauth_token)
+            ->setTwitterSecret($reply->oauth_token_secret)
+        ;
+
+        $this->getDoctrine()->getManager()->flush();
+
+        return $city;
     }
 }

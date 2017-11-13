ceb7fef2484217b3e16be6392cb3aa5c65cf111a
Malte Hübner <maltehuebner@gmx.org>
2017-11-13 19:30:12 +0100

    Show comments in sidebar.

--- src/AppBundle/Controller/SidebarController.php
+++ src/AppBundle/Controller/SidebarController.php
@@ -2,15 +2,19 @@
 
 namespace AppBundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use AppBundle\Entity\Comment;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\User\UserInterface;
 
-class SidebarController extends Controller
+class SidebarController extends AbstractController
 {
     public function sidebarAction(Request $request, UserInterface $user = null): Response
     {
-        return $this->render('AppBundle:Sidebar:sidebar.html.twig');
+        $commentList = $this->getDoctrine()->getRepository(Comment::class)->findLatest($this->getCity($request), 10);
+
+        return $this->render('AppBundle:Sidebar:sidebar.html.twig', [
+            'commentList' => $commentList,
+        ]);
     }
 }

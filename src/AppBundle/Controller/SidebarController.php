17a4a9b6d6174a9344a8edb509b3f6ffe3270087
Malte Hübner <maltehuebner@gmx.org>
2017-11-15 18:26:02 +0100

    Prepare to use photo entity.

--- src/AppBundle/Controller/SidebarController.php
+++ src/AppBundle/Controller/SidebarController.php
@@ -4,13 +4,14 @@ namespace AppBundle\Controller;
 
 use AppBundle\Entity\Comment;
 use AppBundle\Entity\Favorite;
+use AppBundle\Entity\Photo;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 class SidebarController extends AbstractController
 {
-    public function sidebarAction(Request $request, UserInterface $user = null): Response
+    public function sidebarAction(Request $request, UserInterface $user = null, Photo $photo = null): Response
     {
         $commentList = $this->getDoctrine()->getRepository(Comment::class)->findLatest($this->getCity($request), 10);
         $favouriteList = $this->getDoctrine()->getRepository(Favorite::class)->findLatest($this->getCity($request), 10);

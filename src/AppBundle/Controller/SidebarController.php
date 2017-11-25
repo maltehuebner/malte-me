1b77add1441a644ddb5eb6511406823acbbaad34
Malte Hübner <maltehuebner@gmx.org>
2017-11-25 16:12:50 +0100

    Show critical mass date and location in sidebar.

--- src/AppBundle/Controller/SidebarController.php
+++ src/AppBundle/Controller/SidebarController.php
@@ -5,6 +5,8 @@ namespace AppBundle\Controller;
 use AppBundle\Entity\Comment;
 use AppBundle\Entity\Favorite;
 use AppBundle\Entity\Photo;
+use AppBundle\Model\CriticalmassModel;
+use Symfony\Component\Cache\Adapter\RedisAdapter;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\User\UserInterface;
@@ -15,10 +17,27 @@ class SidebarController extends AbstractController
     {
         $commentList = $this->getDoctrine()->getRepository(Comment::class)->findLatest($this->getCity($request), 10);
         $favouriteList = $this->getDoctrine()->getRepository(Favorite::class)->findLatest($this->getCity($request), 10);
+        $criticalmass = $this->getCriticalmass($request);
 
         return $this->render('AppBundle:Sidebar:sidebar.html.twig', [
             'commentList' => $commentList,
             'favouriteList' => $favouriteList,
+            'criticalmass' => $criticalmass,
         ]);
+    }
+
+    protected function getCriticalmass(Request $request): ?CriticalmassModel
+    {
+        $redisConnection = RedisAdapter::createConnection('redis://localhost');
+
+        $cache = new RedisAdapter(
+            $redisConnection,
+            $namespace = '',
+            $defaultLifetime = 0
+        );
+
+        $cacheItem = $cache->getItem(sprintf('criticalmass-%s', 'hamburg'));
+
+        return $cacheItem->get();
     }
 }

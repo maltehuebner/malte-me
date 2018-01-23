26c470408dd1d1ad857b20b2d6b5ebb087098c46
Malte Hübner <maltehuebner@gmx.org>
2018-01-23 18:08:32 +0100

    Add linkedin.

--- src/AppBundle/Share/Network/LinkedInShareNetwork.php
+++ src/AppBundle/Share/Network/LinkedInShareNetwork.php
@@ -2,7 +2,22 @@
 
 namespace AppBundle\Share\Network;
 
+use AppBundle\Entity\Photo;
+
 class LinkedInShareNetwork extends AbstractShareNetwork
 {
+    protected $name = 'LinkedIn';
 
+    protected $icon = 'fa-linkedin';
+
+    protected $backgroundColor = 'rgb(0, 122, 182)';
+
+    protected $textColor = 'white';
+
+    public function createUrlForPhoto(Photo $photo): string
+    {
+        $linkedinShareUrl = 'https://www.linkedin.com/cws/share?&url=%s';
+
+        return sprintf($linkedinShareUrl, urlencode($this->getPhotoUrl($photo)));
+    }
 }

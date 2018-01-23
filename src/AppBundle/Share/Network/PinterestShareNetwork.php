80e97be7affdd88c3e75aaf95068317850caf14b
Malte Hübner <maltehuebner@gmx.org>
2018-01-23 18:09:01 +0100

    Add pinterest.

--- src/AppBundle/Share/Network/PinterestShareNetwork.php
+++ src/AppBundle/Share/Network/PinterestShareNetwork.php
@@ -2,7 +2,22 @@
 
 namespace AppBundle\Share\Network;
 
+use AppBundle\Entity\Photo;
+
 class PinterestShareNetwork extends AbstractShareNetwork
 {
+    protected $name = 'Pinterest';
 
+    protected $icon = 'fa-pinterest';
+
+    protected $backgroundColor = 'rgb(189, 33, 37)';
+
+    protected $textColor = 'white';
+
+    public function createUrlForPhoto(Photo $photo): string
+    {
+        $pinterestShareUrl = 'https://www.pinterest.com/pin/create/link?url=%s&description=%s';
+
+        return sprintf($pinterestShareUrl, urlencode($this->getPhotoUrl($photo)), urlencode($photo->getTitle()));
+    }
 }

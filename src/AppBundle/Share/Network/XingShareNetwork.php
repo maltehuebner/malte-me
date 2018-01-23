1df1ef591b9875fc9527dee2a453bd2650a4d1f1
Malte Hübner <maltehuebner@gmx.org>
2018-01-23 17:45:44 +0100

    Add xing.

--- src/AppBundle/Share/Network/XingShareNetwork.php
+++ src/AppBundle/Share/Network/XingShareNetwork.php
@@ -2,7 +2,22 @@
 
 namespace AppBundle\Share\Network;
 
+use AppBundle\Entity\Photo;
+
 class XingShareNetwork extends AbstractShareNetwork
 {
+    protected $name = 'XING';
 
+    protected $icon = 'fa-xing';
+
+    protected $backgroundColor = 'rgb(1, 101, 104)';
+
+    protected $textColor = 'white';
+
+    public function createUrlForPhoto(Photo $photo): string
+    {
+        $xingShareUrl = 'https://www.xing.com/spi/shares/new?&url=%s';
+
+        return sprintf($xingShareUrl, urlencode($this->getPhotoUrl($photo)));
+    }
 }

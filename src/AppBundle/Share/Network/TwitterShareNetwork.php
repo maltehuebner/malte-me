9008ede24105d5438ce555c16e0ef470229c5ac6
Malte Hübner <maltehuebner@gmx.org>
2018-01-23 15:21:30 +0100

    WIP for twitter.

--- src/AppBundle/Share/Network/TwitterShareNetwork.php
+++ src/AppBundle/Share/Network/TwitterShareNetwork.php
@@ -2,7 +2,14 @@
 
 namespace AppBundle\Share\Network;
 
+use AppBundle\Entity\Photo;
+
 class TwitterShareNetwork extends AbstractShareNetwork
 {
+    public function createUrlForPhoto(Photo $photo): string
+    {
+        $twitterShareUrl = 'https://twitter.com/intent/tweet?text=%s&url=%s';
 
+        return sprintf($twitterShareUrl, urlencode($photo->getTitle()), urlencode($this->getPhotoUrl($photo)));
+    }
 }

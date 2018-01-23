a61ec871d88abe0521be12bb4b88ee38f82a15e9
Malte Hübner <maltehuebner@gmx.org>
2018-01-23 18:08:46 +0100

    Add whatsapp.

--- src/AppBundle/Share/Network/WhatsappShareNetwork.php
+++ src/AppBundle/Share/Network/WhatsappShareNetwork.php
@@ -2,7 +2,24 @@
 
 namespace AppBundle\Share\Network;
 
+use AppBundle\Entity\Photo;
+
 class WhatsappShareNetwork extends AbstractShareNetwork
 {
+    protected $name = 'WhatsApp';
 
+    protected $icon = 'fa-whatsapp';
+
+    protected $backgroundColor = 'rgb(37, 211, 102)';
+
+    protected $textColor = 'white';
+
+    public function createUrlForPhoto(Photo $photo): string
+    {
+        $whatsappShareUrl = 'whatsapp://send?text=%s';
+
+        $text = sprintf('%s%20%s', $this->getPhotoUrl($photo), $photo->getTitle());
+
+        return sprintf($whatsappShareUrl, $text);
+    }
 }

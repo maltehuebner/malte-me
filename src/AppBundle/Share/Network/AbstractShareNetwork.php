831b6d50f481ddc76b312c5c60a5f3f5f385525c
Malte Hübner <maltehuebner@gmx.org>
2018-01-23 15:18:29 +0100

    WIP.

--- src/AppBundle/Share/Network/AbstractShareNetwork.php
+++ src/AppBundle/Share/Network/AbstractShareNetwork.php
@@ -4,6 +4,7 @@ namespace AppBundle\Share\Network;
 
 use AppBundle\Entity\Photo;
 use Symfony\Bundle\FrameworkBundle\Routing\Router;
+use Symfony\Component\Routing\RouterInterface;
 
 abstract class AbstractShareNetwork implements ShareNetworkInterface
 {
@@ -31,6 +32,13 @@ abstract class AbstractShareNetwork implements ShareNetworkInterface
         $identifier = strtolower(str_replace('ShareNetwork', '', $shortname));
 
         return $identifier;
+    }
+
+    protected function getPhotoUrl(Photo $photo): string
+    {
+        $photoUrl = $this->router->generate('show_photo', ['slug' => $photo->getSlug()], RouterInterface::ABSOLUTE_URL);
+
+        return str_replace('http://', 'https://', $photoUrl);
     }
 
     public function createUrlForPhoto(Photo $photo): string
--- src/AppBundle/Share/Network/FacebookShareNetwork.php
+++ src/AppBundle/Share/Network/FacebookShareNetwork.php
@@ -3,17 +3,13 @@
 namespace AppBundle\Share\Network;
 
 use AppBundle\Entity\Photo;
-use Symfony\Component\Routing\RouterInterface;
 
 class FacebookShareNetwork extends AbstractShareNetwork
 {
     public function createUrlForPhoto(Photo $photo): string
     {
-        $photoUrl = str_replace('http://', 'https://', $this->router->generate('show_photo', ['slug' => $photo->getSlug()], RouterInterface::ABSOLUTE_URL));
-        $photoTitle = $photo->getTitle();
-
         $facebookShareUrl = 'https://www.facebook.com/sharer.php?u=%s&t=%s';
 
-        return sprintf($facebookShareUrl, urlencode($photoUrl), urlencode($photoTitle));
+        return sprintf($facebookShareUrl, urlencode($this->getPhotoUrl($photo)), urlencode($photo->getTitle()));
     }
 }

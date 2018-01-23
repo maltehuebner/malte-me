2b73edd7282ace66a4a73ee219be9bc12320f228
Malte Hübner <maltehuebner@gmx.org>
2018-01-23 18:09:08 +0100

    Add reddit.

--- src/AppBundle/Share/Network/RedditShareNetwork.php
+++ src/AppBundle/Share/Network/RedditShareNetwork.php
@@ -2,7 +2,22 @@
 
 namespace AppBundle\Share\Network;
 
+use AppBundle\Entity\Photo;
+
 class RedditShareNetwork extends AbstractShareNetwork
 {
+    protected $name = 'Reddit';
 
+    protected $icon = 'fa-reddit';
+
+    protected $backgroundColor = 'rgb(255, 69, 1)';
+
+    protected $textColor = 'white';
+
+    public function createUrlForPhoto(Photo $photo): string
+    {
+        $redditShareUrl = 'https://ssl.reddit.com/submit?url=%s';
+
+        return sprintf($redditShareUrl, urlencode($this->getPhotoUrl($photo)));
+    }
 }

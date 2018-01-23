2316ffd9850862a8f465e9b7318d0ee9cd89d6a7
Malte Hübner <maltehuebner@gmx.org>
2018-01-23 16:50:19 +0100

    Add Google plus.

--- src/AppBundle/Share/Network/GoogleShareNetwork.php
+++ src/AppBundle/Share/Network/GoogleShareNetwork.php
@@ -2,7 +2,23 @@
 
 namespace AppBundle\Share\Network;
 
+use AppBundle\Entity\Photo;
+
 class GoogleShareNetwork extends AbstractShareNetwork
 {
+    protected $name = 'Google+';
 
+    protected $icon = 'fa-google-plus';
+
+    protected $backgroundColor = 'rgb(220, 78, 65)';
+
+    protected $textColor = 'white';
+
+    public function createUrlForPhoto(Photo $photo): string
+    {
+        $googleShareUrl = 'https://plus.google.com/share?url=%s';
+
+        return sprintf($googleShareUrl, urlencode($this->getPhotoUrl($photo)));
+    }
 }
+

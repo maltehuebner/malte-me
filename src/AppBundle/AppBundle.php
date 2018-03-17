9a02d47140efcadad4414e55e9f2e51296f7d72b
Malte Hübner <maltehuebner@gmx.org>
2018-03-17 14:00:51 +0100

    Fix widget services.

--- src/AppBundle/AppBundle.php
+++ src/AppBundle/AppBundle.php
@@ -5,6 +5,7 @@ namespace AppBundle;
 use AppBundle\DependencyInjection\Compiler\ShareNetworkPass;
 use AppBundle\DependencyInjection\Compiler\WidgetPass;
 use AppBundle\Share\Network\ShareNetworkInterface;
+use AppBundle\Widget\WidgetFactoryInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
@@ -17,8 +18,7 @@ class AppBundle extends Bundle
         $container->addCompilerPass(new WidgetPass());
         $container->addCompilerPass(new ShareNetworkPass());
 
-        $container->registerForAutoconfiguration(ShareNetworkInterface::class)
-            ->addTag('share.network')
-        ;
+        $container->registerForAutoconfiguration(ShareNetworkInterface::class)->addTag('share.network');
+        $container->registerForAutoconfiguration(WidgetFactoryInterface::class)->addTag('widget.factory');
     }
 }

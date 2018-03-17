448c0a9aa69d2552a49de10e34f9e4726eec774f
Malte Hübner <maltehuebner@gmx.org>
2018-03-17 13:57:15 +0100

    Fix all share network services.

--- src/AppBundle/AppBundle.php
+++ src/AppBundle/AppBundle.php
@@ -4,6 +4,7 @@ namespace AppBundle;
 
 use AppBundle\DependencyInjection\Compiler\ShareNetworkPass;
 use AppBundle\DependencyInjection\Compiler\WidgetPass;
+use AppBundle\Share\Network\ShareNetworkInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
@@ -15,5 +16,9 @@ class AppBundle extends Bundle
 
         $container->addCompilerPass(new WidgetPass());
         $container->addCompilerPass(new ShareNetworkPass());
+
+        $container->registerForAutoconfiguration(ShareNetworkInterface::class)
+            ->addTag('share.network')
+        ;
     }
 }

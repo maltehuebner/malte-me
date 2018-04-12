978e66b9629acfed40f18cafab2982d792f2580d
Malte Hübner <maltehuebner@gmx.org>
2018-04-12 10:52:34 +0200

    There is a quite simpler url for data source.

--- src/AppBundle/BikeMeter/DataFetcher.php
+++ src/AppBundle/BikeMeter/DataFetcher.php
@@ -7,7 +7,7 @@ use SimpleXMLElement;
 
 class DataFetcher
 {
-    const DATA_SOURCE_URL = 'https://geoportal-hamburg.de/geodienste_hamburg_de/HH_WMS_Radverkehrszaehlsaeulen?SERVICE=WMS&VERSION=1.3.0&REQUEST=GetFeatureInfo&FORMAT=image%2Fpng&TRANSPARENT=true&QUERY_LAYERS=zaehlstellen&t=246&zufall=0.13577735920122191&LAYERS=zaehlstellen&INFO_FORMAT=text%2Fxml&FEATURE_COUNT=1&I=50&J=50&CRS=EPSG%3A25832&STYLES=&WIDTH=101&HEIGHT=101&BBOX=566012.1849068201%2C5934123.9991770405%2C567615.5590409981%2C5935727.373311218';
+    const DATA_SOURCE_URL = 'https://geodienste.hamburg.de/HH_WFS_Radverkehrszaehlsaeulen?SERVICE=WFS&VERSION=1.1.0&REQUEST=GetFeature&typename=app:zaehlsaeulen';
 
     /** @var Curl $curl */
     protected $curl;

1480ff812b3b85cd0e45667406bbe40ef128a645
Malte Hübner <maltehuebner@gmx.org>
2018-04-13 19:39:18 +0200

    Create retriever, entity and migration.

---
+++ src/AppBundle/Entity/WeatherData.php
@@ -0,0 +1,195 @@
+<?php declare(strict_types=1);
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Table(name="weather_data")
+ * @ORM\Entity()
+ */
+class WeatherData
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @var int $id
+     */
+    protected $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="City", inversedBy="weatherDatas")
+     * @ORM\JoinColumn(name="city_id", referencedColumnName="id")
+     * @var City $city
+     */
+    protected $city;
+
+    /**
+     * @ORM\Column(type="float")
+     * @var float $temperaturMin
+     */
+    protected $temperaturMin;
+
+    /**
+     * @ORM\Column(type="float")
+     * @var float $temperaturMax
+     */
+    protected $temperaturMax;
+
+    /**
+     * @ORM\Column(type="float")
+     * @var float $windSpeed
+     */
+    protected $windSpeed;
+
+    /**
+     * @ORM\Column(type="float")
+     * @var float $windDirection
+     */
+    protected $windDirection;
+
+    /**
+     * @ORM\Column(type="float")
+     * @var float $clouds
+     */
+    protected $clouds;
+
+    /**
+     * @ORM\Column(type="float")
+     * @var string $weather
+     */
+    protected $weather;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=false)
+     * @var \DateTime $dateTime
+     */
+    protected $dateTime;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=false)
+     * @var \DateTime $createdAt
+     */
+    protected $createdAt;
+
+    public function __construct()
+    {
+        $this->createdAt = new \DateTime();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): WeatherData
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function getDateTime(): \DateTime
+    {
+        return $this->dateTime;
+    }
+
+    public function setDateTime(\DateTime $dateTime): WeatherData
+    {
+        $this->dateTime = $dateTime;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): \DateTime
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTime $createdAt): WeatherData
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function setCity(City $city): WeatherData
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+
+    public function getTemperaturMin(): ?float
+    {
+        return $this->temperaturMin;
+    }
+
+    public function setTemperaturMin(float $temperaturMin = null): WeatherData
+    {
+        $this->temperaturMin = $temperaturMin;
+
+        return $this;
+    }
+
+    public function getTemperaturMax(): ?float
+    {
+        return $this->temperaturMax;
+    }
+
+    public function setTemperaturMax(float $temperaturMax = null): WeatherData
+    {
+        $this->temperaturMax = $temperaturMax;
+
+        return $this;
+    }
+
+    public function getWindSpeed(): ?float
+    {
+        return $this->windSpeed;
+    }
+
+    public function setWindSpeed(float $windSpeed = null): WeatherData
+    {
+        $this->windSpeed = $windSpeed;
+
+        return $this;
+    }
+
+    public function getWindDirection(): ?float
+    {
+        return $this->windDirection;
+    }
+
+    public function setWindDirection(float $windDirection = null): WeatherData
+    {
+        $this->windDirection = $windDirection;
+
+        return $this;
+    }
+
+    public function getClouds(): ?float
+    {
+        return $this->clouds;
+    }
+
+    public function setClouds(float $clouds = null): WeatherData
+    {
+        $this->clouds = $clouds;
+
+        return $this;
+    }
+
+    public function getWeather(): ?string
+    {
+        return $this->weather;
+    }
+
+    public function setWeather(string $weather = null): WeatherData
+    {
+        $this->weather = $weather;
+
+        return $this;
+    }
+}
---
+++ src/AppBundle/WeatherRetriever/WeatherRetriever.php
@@ -0,0 +1,82 @@
+<?php declare(strict_types=1);
+
+namespace AppBundle\WeatherRetriever;
+
+use AppBundle\Entity\City;
+use AppBundle\Entity\WeatherData;
+use Cmfcmf\OpenWeatherMap;
+use Cmfcmf\OpenWeatherMap\CurrentWeather;
+use Doctrine\Bundle\DoctrineBundle\Registry as Doctrine;
+
+class WeatherRetriever
+{
+    /** @var Doctrine $doctrine */
+    protected $doctrine;
+
+    /** @var City $city */
+    protected $city;
+
+    /** @var OpenWeatherMap $openWeatherMap */
+    protected $openWeatherMap;
+
+    /** @var WeatherData $weatherData */
+    protected $weatherData;
+
+    public function __construct(Doctrine $doctrine, string $openweathermapApiKey)
+    {
+        $this->doctrine = $doctrine;
+
+        $this->openWeatherMap = new OpenWeatherMap($openweathermapApiKey);
+    }
+
+    public function setCity(City $city): WeatherRetriever
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+
+    protected function fetch(): WeatherRetriever
+    {
+        $currentWeather = $this->retrieveWeather();
+        $this->weatherData = $this->createWeatherData($currentWeather);
+
+        return $this;
+    }
+
+    public function getWeatherData(): WeatherData
+    {
+        return $this->weatherData;
+    }
+
+    protected function retrieveWeather(): CurrentWeather
+    {
+        $lang = 'de';
+        $units = 'metric';
+
+        $latLng = [
+            'lat' => $this->city->getLatitude(),
+            'lon' => $this->city->getLongitude(),
+        ];
+
+        $weather = $this->openWeatherMap->getWeather($latLng, $units, $lang);
+
+        return $weather;
+    }
+
+    protected function createWeatherData(CurrentWeather $currentWeather): WeatherData
+    {
+        $weatherData = new WeatherData();
+
+        $weatherData
+            ->setCity($this->city)
+            ->setTemperaturMin($currentWeather->temperature->min->getValue())
+            ->setTemperaturMax($currentWeather->temperature->max->getValue())
+            ->setClouds($currentWeather->clouds->getValue())
+            ->setWindDirection($currentWeather->wind->direction->getValue())
+            ->setWindSpeed($currentWeather->wind->speed->getValue())
+            ->setWeather($currentWeather->weather->description);
+
+        return $weatherData;
+    }
+}
